5faac54279f814e527603f3daf1a0842ae30a891
Matthieu Gourdon <matthieu.gourdon@tpg06.cpe.lan>
2022-12-07 17:01:12 +0100

    working react template of card in line and in full render

---
+++ elegant-card-game/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LineCard from './components/card/components/LineCard';
+import SimpleCard from './components/card/components/SimpleCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleCard img_src='https://cdn.icon-icons.com/icons2/896/PNG/512/pokemon_go_play_game_cinema_film_movie_icon-icons.com_69163.png' name="coucou" price={1000} />
+    </div>
+  );
+}
+
+export default App;
