851ff07547d7b3dcdd3f6b93654b49799a57073a
julesfiliot <jules.filiot@cpe.fr>
2022-12-17 17:10:23 +0100

    [ADD] loader component

--- src/components/fight/components/Wait.jsx
+++ src/components/fight/components/Wait.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Loader from '../../card/components/Loader';
 
 export default function Wait() {
   return (
     <div className="waitingContainer">
-      WAIT
+      <Loader />
     </div>
   );
 }
