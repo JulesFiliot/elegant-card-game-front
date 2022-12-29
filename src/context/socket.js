aab1f91bb17b13310b02bda55e0e778ba27f8a59
JulesFiliot <jules.filiot@cpe.fr>
2022-12-29 05:11:51 +0100

    [CHANGE] api path

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import socketio from 'socket.io-client';
 
-export const chatSocket = socketio.connect(process.env.REACT_APP_CHAT_URL);
+export const chatSocket = socketio.connect(process.env.REACT_APP_CHAT_URL, { path: '/chatws/' });
 export const notifierSocket = socketio.connect(process.env.REACT_APP_NOTIFIER_URL, { path: '/notifierws/' });
 export const SocketContext = React.createContext();
